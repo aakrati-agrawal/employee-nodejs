3019c0c65f3806d5a7794f924e39728f764bdaa4
Aakrati Agrawal <aa01.nitc@gmail.com>
2024-03-13 21:24:58 +0530

    Updated app and app manifests

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 // Create connection
 const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
+    //port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
